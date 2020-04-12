e34c3902bdf750693f565a82e1374c00585235f7
Chansol Lim <chansollim1994@berkeley.edu>
2020-04-11 21:54:18 -0700

    created an initial project

--- coronaValon/coronaValon/SceneDelegate.swift
+++ coronaValon/coronaValon/SceneDelegate.swift
@@ -16,8 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
+        let startViewController = StartViewController()
+        window?.rootViewController = startViewController
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
     }
